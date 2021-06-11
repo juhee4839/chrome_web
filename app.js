87efe1d17b689eb3825003a5af363288896067be
juhee4839 <79627080+juhee4839@users.noreply.github.com>
2021-06-12 00:23:22 +0900

    update
    update

---
+++ app.js
@@ -0,0 +1,15 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+
+
+function onLoginSubmit(event){
+    event.preventDefault();
+    console.log(loginInput.value);
+    }
+
+
+loginForm.addEventListener("submit", onLoginSubmit);
+
+document.getElementById
+
+practice!!!
