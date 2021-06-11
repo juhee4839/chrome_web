0ab5d83a504712518aca04e3d21af24a97207822
juhee4839 <79627080+juhee4839@users.noreply.github.com>
2021-06-12 00:58:35 +0900

    saving username
    saving username

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
+
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event){
     event.preventDefault();
-    console.log(loginInput.value);
+    loginForm.classList.add("hidden"); 
+    const username = loginInput.value;
+    greeting.innerText ="Hello" + username;
+    greeting.innerText =`Hello ${username} ;
+    greeting.classList.remove(HIDDEN_CLASSNAME)
     }
 
 
 loginForm.addEventListener("submit", onLoginSubmit);
-
-document.getElementById
-
-practice!!!!!!!!!!!
