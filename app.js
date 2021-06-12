70a36c8b127a10e51a01a234331104fb40261713
juhee4839 <79627080+juhee4839@users.noreply.github.com>
2021-06-13 00:53:31 +0900

    saving username
    saving username

--- app.js
+++ app.js
@@ -4,15 +4,27 @@ const greeting = document.querySelector("#greeting");
 
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event){
     event.preventDefault();
     loginForm.classList.add("hidden"); 
     const username = loginInput.value;
-    greeting.innerText ="Hello" + username;
-    greeting.innerText =`Hello ${username} ;
-    greeting.classList.remove(HIDDEN_CLASSNAME)
+    localStorage.setItem(USERNAME_KEY, username)
+    paintGreetings(username);
+    }
+
+    function paintGreetings(username){
+        greeting.innerText = `Hello ${username}`
+        greeting.classList.remove(HIDDEN_CLASSNAME);
     }
 
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}else{
+    paintGreetings(savedUsername);
+    }
