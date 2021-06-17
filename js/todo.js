be637f246025634940f656ac1ea80a687529d787
juhee4839 <79627080+juhee4839@users.noreply.github.com>
2021-06-17 23:44:23 +0900

    loading to Dos
    loading to Dos

--- js/todo.js
+++ js/todo.js
@@ -2,9 +2,16 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
-function deleteToDo(event) {
-    console.log(event.target.parentElement.innerText);
+const TODOS_KEY = "todos"
+const toDos = [];
 
+function saveToDos() {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+function deleteToDo(event) {
+    const li = event.target.parentElement;
+    li.remove();
 }
 
 function paintToDo(newTodo) {
@@ -24,8 +31,18 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    toDos.push(newTodo);
     paintToDo(newTodo);
+    saveToDos();
 
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+console.log(savedToDos);
+if(savedToDos){
+    const parsedToDos = JSON.parse(savedToDos); 
+    parsedToDos.forEach((item) => console.log("this is  the turn of",item));
+}
