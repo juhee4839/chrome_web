e8f3a641415ddbc8c18fe2428a174118868bc475
juhee4839 <79627080+juhee4839@users.noreply.github.com>
2021-06-22 13:26:00 +0900

    adding CSS
    adding CSS

--- js/weather.js
+++ js/weather.js
@@ -12,7 +12,6 @@ function onGeoOk(position){
         const city = document.querySelector("#weather span:last-child");
         city.innerText = data.name;
         weather.innerText = `${data.weather[0].main}` / `${data.main.temp}`
-
     });
     }
 
